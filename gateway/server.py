ac08bfe68f73ea292520fb341a9afd39a3145993
bhavasagar09 <bhavasagar.putta@quantrium.ai>
2024-04-25 23:35:46 +0530

    added notification service

--- gateway/server.py
+++ gateway/server.py
@@ -1,24 +1,24 @@
-import os, pika, gridfs, json
-from flask import Flask, request
-from flask_pymongo import PyMongo
+import os, pika, gridfs, json, base64
+from flask import Flask, request, send_file
+from flask_pymongo import PyMongo, ObjectId
 from auth_svc import access
 from auth import validate
 from storage import utils
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://host.minikube.internal:27017/videos"
 
-mongo = PyMongo(app)
-fs = gridfs.GridFS(mongo.db)
+video_mongo = PyMongo(app, uri="mongodb://host.minikube.internal:27017/videos")
+mp3s_mongo = PyMongo(app, uri="mongodb://host.minikube.internal:27017/mp3s")
+
+video_fs = gridfs.GridFS(video_mongo.db)
+mp3s_fs = gridfs.GridFS(mp3s_mongo.db)
 
 connection = pika.BlockingConnection(pika.ConnectionParameters("rabbitmq"))
 channel = connection.channel()
 
 @app.post("/v1/login")
 def login():
-    print("STARTING LOGIN")
     token, err = access.login(request)
-    print(token, err, "Logging data")
     if err:
         return err
     return token, 200
@@ -39,15 +39,25 @@ def upload():
     if not (file.filename and file.filename.endswith('.mp4')):
         return "Please upload and mp4 formatted file", 400
 
-    err = utils.upload(claims, file, fs, channel)
+    err = utils.upload(claims, file, video_fs, channel)
     if err:
         return err
 
     return "Uploaded Succesfully", 200
 
-@app.post("/v1/download")
+@app.get("/v1/download")
 def download():
-    pass
+    if not request.args.get("fid"):
+        return "File id is required"
+    
+    fid = base64.urlsafe_b64decode(request.args.get("fid"))
+    try:
+        out = mp3s_fs.get(ObjectId(fid))
+        return send_file(out, download_name=f"{fid}.mp3")
+    except Exception as e:
+        print(e)
+        return "Internal server error", 500
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
